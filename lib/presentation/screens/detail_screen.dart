baf2086d639d036f172c30692580de35a1ced8e7
droidbook <david.aleksian@phystech.edu>
2025-04-15 19:10:27 +0300

    cosmetic fixes in detail screen

--- lib/presentation/screens/detail_screen.dart
+++ lib/presentation/screens/detail_screen.dart
@@ -9,75 +9,101 @@ class CatDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.all(16.0),
-      padding: const EdgeInsets.all(16.0),
+      clipBehavior: Clip.hardEdge,
+      margin: const EdgeInsets.only(
+        top: 100.0,
+        left: 16.0,
+        right: 16.0,
+        bottom: 150.0,
+      ),
       decoration: BoxDecoration(
         color: Colors.white,
         borderRadius: BorderRadius.circular(16.0),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withValues(alpha: .4),
+            spreadRadius: 5,
+            blurRadius: 7,
+            offset: const Offset(0, 3),
+          ),
+        ],
       ),
       child: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           mainAxisSize: MainAxisSize.min,
+
           children: [
-            Align(
-              alignment: Alignment.topLeft,
-              child: IconButton(
-                icon: const Icon(Icons.close, size: 30),
-                onPressed: () => Navigator.of(context).pop(),
+            Image.network(cat.url),
+            const SizedBox(height: 7.0),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Text(
+                cat.breedName,
+                style: const TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
-            ClipRRect(
-              borderRadius: BorderRadius.circular(16.0),
-              child: Image.network(cat.url),
+            const SizedBox(height: 3.0),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Text(
+                cat.description,
+                style: const TextStyle(fontSize: 16, color: Colors.grey),
+              ),
             ),
-            const SizedBox(height: 16.0),
-            Text(
-              cat.breedName,
-              style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 8.0),
-            Text(
-              cat.description,
-              style: const TextStyle(fontSize: 16, color: Colors.grey),
-            ),
-            const SizedBox(height: 16.0),
-            const Wrap(
-              children: [
-                Icon(Icons.location_on, color: Colors.red),
-                SizedBox(width: 5),
-                Text(
-                  'Origin: ',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 15,
-                    fontWeight: FontWeight.bold,
+            const SizedBox(height: 10.0),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: const Wrap(
+                children: [
+                  Icon(Icons.location_on, color: Colors.red),
+                  SizedBox(width: 3),
+                  Text(
+                    'Origin: ',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-            Text(
-              cat.origin,
-              style: const TextStyle(color: Colors.grey, fontSize: 15),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Text(
+                cat.origin,
+                style: const TextStyle(color: Colors.grey, fontSize: 15),
+              ),
             ),
-            const SizedBox(height: 8.0),
-            const Wrap(
-              children: [
-                Icon(Icons.pets, color: Colors.blue),
-                SizedBox(width: 5),
-                Text(
-                  'Temperament: ',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 15,
-                    fontWeight: FontWeight.bold,
+            const SizedBox(height: 11.0),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: const Wrap(
+                children: [
+                  Icon(Icons.pets, color: Colors.blue),
+                  SizedBox(width: 3),
+                  Text(
+                    'Temperament: ',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-            Text(
-              cat.temperament,
-              style: const TextStyle(color: Colors.grey, fontSize: 15),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 16.0),
+              padding: const EdgeInsets.only(bottom: 10.0),
+              child: Text(
+                cat.temperament,
+                style: const TextStyle(color: Colors.grey, fontSize: 15),
+              ),
             ),
           ],
         ),
